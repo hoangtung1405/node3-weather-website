348e823d9cbca11d1a16cea47b5a50aedc5ab922
Tung Phi <hoangtungphi1405@gmail.com>
2020-12-26 22:44:07 +0700

    Add new data to be shown

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+            console.log(response.body.current)
+            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees.")
         }
     })
 }
